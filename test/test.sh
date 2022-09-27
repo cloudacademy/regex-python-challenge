6962bf9dc97f88ea2ca6e33b14347bb5023e2923
rburchill <64294584+rburchill@users.noreply.github.com>
2022-09-27 16:25:21 +1000

    Add shebang and locking

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 (
+        flock -x -w 20 200 || exit 1
+
         check=$1
         file=$2
         expected=$3
@@ -11,4 +15,4 @@
 
         python3 $tmpsrcfile >$tmpoutfile
         grep -q "$expected" $tmpoutfile
-)
+) 200>/tmp/check.exclusivelock

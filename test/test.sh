08943316f9748f98b9da4d35b84560062b2c5780
rburchill <64294584+rburchill@users.noreply.github.com>
2022-09-27 13:37:55 +1000

    Ensure new line between solution and test

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,7 @@ expected=$3
 rm -rf ../check/$check
 mkdir -p ../check/$check
 cp ../src/$file ../check/$check
+echo "\n" >> ../check/$check/$file
 cat ./texts/$check.py >> ../check/$check/$file
 
 python3 ../check/$check/$file > output.txt
